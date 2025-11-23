3e25e886f1cbcf4b2bd88023b5270564d7b5e64b
aoku151 <guratan.151.123@gmail.com>
2025-11-23 12:29:39 +0900

    作業変更するためにCommit

--- sender.py
+++ sender.py
@@ -4,14 +4,13 @@ import uuid
 import logging
 from dotenv import load_dotenv
 from supabase import acreate_client, AsyncClient
-import supabase as Supabase
 import scapi
 import asyncio
 import aiohttp
 import aioconsole
 import json
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord import app_commands
 load_dotenv()
 
@@ -19,12 +18,12 @@ sp_url: str = os.getenv("SUPABASE_URL")
 sp_key: str = os.getenv("SUPABASE_ANON_KEY")
 sc_name: str = os.getenv("SCRATCH_USER")
 sc_pass: str = os.getenv("SCRATCH_PASSWORD")
-DISCORD_TOKEN: str = os.getenv("DISCORD_TOKEN")
+DISCORD_TOKEN: str = os.getenv("DISCORD_TOKEN_SUB")
 nx_auth_url = "https://mnvdpvsivqqbzbtjtpws.supabase.co/functions/v1/scratch-auth-handler"
 sessions_path = "sessions.json"
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="s!", intents=intents)
 
 main_log = logging.getLogger("Main")
 main_log.setLevel(level=logging.DEBUG)
@@ -45,15 +44,14 @@ def setSession(key, value):
     with open(sessions_path, "w") as f:
         json.dump(data, f, indent=4)
 
-async def get_supabase():
+async def get_supabase() -> AsyncClient:
     log = get_log("get_supabase")
     try:
         supabase: AsyncClient = await acreate_client(sp_url, sp_key)
         sp_jwt = getSession("sp_key")
-        sp_res = await supabase.auth.set_session(sp_jwt, sp_jwt)
+        await supabase.auth.set_session(sp_jwt, sp_jwt)
         log.info("セッションは有効です。認証に成功しました!")
-        session = sp_res.session
-        return supabase, session
+        return supabase
     except Exception:
         try:
             log.warning("セッションの有効期限が切れているので、新しいセッションを作成します...")
@@ -73,12 +71,11 @@ async def get_supabase():
                 async with session.post(nx_auth_url, json=second, headers=header) as res:
                     response = await res.json()
             log.info("セッションを取得しました!")
-            sp_res = await supabase.auth.set_session(response['jwt'], response['jwt'])
+            await supabase.auth.set_session(response['jwt'], response['jwt'])
             setSession("sp_key", response['jwt'])
             log.info("セッションは有効です。認証に成功しました!")
             await sc.client_close()
-            session = sp_res.session
-            return supabase, session
+            return supabase
         except Exception as e:
             log.error(f"NyaXのログイン中にエラーが発生しました。\n{e}")
 async def get_scratch():
@@ -127,47 +124,23 @@ async def get_currentUser(supabase: AsyncClient):
 
 log_channel: discord.TextChannel = None
 
+messages = []
+
+@tasks.loop(seconds=60)
+async def send_nyax():
+    global messages
+    log = get_log("send_nyax")
+    try:
+        mes = ""
+        for i in messages:
+            
+
 async def main():
     log = main_log
-    global currentUser, session
+    global currentUser, session, messages
     try:
-        supabase, session = await get_supabase()
-        # session = await supabase.auth.get_session()
-        currentUser = await get_currentUser(supabase)
-        log.debug(currentUser)
-
-        async def handle_notification():
-            global currentUser
-            try:
-                session = await supabase.auth.get_session()
-                response = (
-                    await supabase.table("user")
-                    .select("notice")
-                    .eq("id", currentUser["id"])
-                    .execute()
-                )
-                notices = response.notice
-                for n_obj in notices:
-                    notification = None
-                    if(type(n_obj) is dict):
-                        notification = n_obj
-                    else:
-                        notification = {"id": uuid.uuid4(), "message": n_obj, "open": "", "click": True}
-                    if(!notification.click):
-                        await log_channel.send(notification.message)
-                await supabase.rpc('mark_all_notifications_as_read', {"p_user_id":currentUser["id"]})
-                if(currentUser.notice):
-                    for i in currentUser.notice:
-                        i["click"] = True
-                currentUser["notice_count"] = 0
-            except Exception as e:
-                log.error(f"通知の取得にエラーが発生しました。{e}")
-
-        subscribe1 = (
-            await supabase.channel("nyax-feed")
-            .on_postgres_changes("UPDATE", schema="public", table="user", filter=f"id=eq.{currentUser['id']}", callback=handle_notification)
-            .subscribe()
-        )
+        supabase: AsyncClient = await get_supabase()
+        session = await supabase.auth.get_session()
 
         @bot.event
         async def on_ready():
@@ -180,7 +153,13 @@ async def main():
             except Exception as e:
                 log.error(f"コマンドの同期中にエラーが発生しました。\n{e}")
             asyncio.create_task(console_input())
-            await supabase.rpc("create_post", {"p_content": "NyaXBotが起動しました!", "p_reply_id": None, "p_repost_to": None, "p_attachments": None}).execute()
+            # await supabase.rpc("create_post", {"p_content": "NyaXBotが起動しました!", "p_reply_id": None, "p_repost_to": None, "p_attachments": None}).execute()
+        @bot.event
+        async def on_message(message:discord.Message):
+            if message.channel == log_channel:
+                messages.append(message.content)
+            await bot.process_commands(message)
+
         await bot.start(DISCORD_TOKEN)
     except Exception as e:
         log.error(f"BOTの起動中にエラーが発生しました\n{e}")
--- test_d.py
+++ test_d.py
@@ -1,18 +1,33 @@
 import discord
 from discord.ext import commands
 from os import getenv
+import asyncio
+import aioconsole
 from dotenv import load_dotenv
 load_dotenv()
 
 intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix="!", intents=intents)
-@bot.event
-async def on_ready():
-    for i in bot.guilds :
-        print(i.id)
-        for a in i.channels :
-            print(a.name)
-            print(a.id)
 
-bot.run(getenv("DISCORD_TOKEN"))
+async def console_input():
+    while True:
+        line = await aioconsole.ainput("type:")
+        if line.strip() == "finish":
+            await bot.close()
+            break
+
+async def main():
+    @bot.event
+    async def on_ready():
+        for i in bot.guilds :
+            print(i.name)
+            print(i.id)
+            for a in i.channels :
+                print(a.name)
+                print(a.id)
+    await bot.start(getenv("DISCORD_TOKEN"))
+
+discord.utils.setup_logging()
+if __name__ == "__main__":
+    asyncio.run(main())
