3e92a2244c24aca2a036d4c384c9267a516a7555
あおく <guratan.151.123@gmail.com>
2025-11-27 17:04:07 +0900

    Update log.py

--- func/log.py
+++ func/log.py
@@ -4,5 +4,6 @@ def get_log(name:str):
   log = logging.getLogger(name)
   log.setLevel(level=logging.DEBUG)
   return log
-stream_hancler = logging.StreamHandler()
-logging.getLogger("realtime")
+stream_handler = logging.StreamHandler()
+logging.getLogger("realtime._async.channel").setLevel(logging.WARNING)
+logging.getLogger("realtime._async.client").setLevel(logging.WARNING)
