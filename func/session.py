752da9643af5092ebb25ed1d91a500e0fb4a7240
あおく <guratan.151.123@gmail.com>
2025-11-27 11:34:23 +0900

    Create session.py

---
+++ func/session.py
@@ -0,0 +1,9 @@
+import os
+import logging
+from dotenv import load_dotenv
+from supabase import acreate_client, AsyncClient
+import supabase as Supabase
+import scapi
+import asyncio
+import aiohttp
+import json
