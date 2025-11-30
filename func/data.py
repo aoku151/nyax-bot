ce04cfe1f8b3e4d76e694edf8fe919c0d3b7d369
aoku151 <guratan.151.123@gmail.com>
2025-11-30 16:11:04 +0900

    DM返信機能&ステータス更新機能

--- func/data.py
+++ func/data.py
@@ -1,10 +1,14 @@
 header = {"Content-Type": "application/json"}
 
-dmCommandList = []
+dmCommandList = [
+    "/hello",
+    "/help"
+]
 
 dmCommands = ""
 if len(dmCommandList) != 0:
-    dmCommands += f"{i}\n"
+    for i in dmCommandList:
+        dmCommands += f"{i}\n"
 
 dmInviteMessage = f"""\
 NyaXBotへのDMが開通しました! _veryx2-long-cat_
@@ -12,5 +16,12 @@ NyaXBotへのDMが開通しました! _veryx2-long-cat_
 Githubリポジトリ: https://github.com/aoku151/nyax-bot/
 
 使用出来るコマンド
-{dmCommands}
-"""
+{dmCommands}"""
+
+helpMessage = f"""\
+NyaXBot @1340
+開発者: @4332
+Githubリポジトリ: https://github.com/aoku151/nyax-bot/
+
+使用できるコマンド
+{dmCommands}"""
--- main.py
+++ main.py
@@ -12,11 +12,11 @@ import aioconsole
 import json
 import discord
 from datetime import datetime, timezone
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord import app_commands
 from func.session import Sessions
 from func.log import get_log, stream_handler
-from func.data import dmInviteMessage
+from func.data import dmInviteMessage, helpMessage
 load_dotenv()
 
 DISCORD_TOKEN: str = os.getenv("DISCORD_TOKEN")
@@ -33,6 +33,7 @@ async def console_input():
         line = await aioconsole.ainput("type:")
         if line.strip() == "finish":
             main_log.info("Stop.")
+            await status_update("停止中")
             await bot.close()
             break
 
@@ -42,32 +43,135 @@ session = None
 
 log_channel: discord.TextChannel = None
 
+console_task = None
+
+async def send_dm_message(dmid:str,message:str):
+    log = get_log("send_dm_message")
+    try:
+        messagedict = {
+            "id": str(uuid.uuid4()),
+            "time": datetime.now(timezone.utc).isoformat(timespec="microseconds"),
+            "userid": currentUser["id"],
+            "content": message,
+            "attachments": [],
+            "read": [currentUser["id"]]
+        }
+        await supabase.rpc("append_to_dm_post", {"dm_id_in": dmid, "new_message_in": messagedict}).execute()
+    except Exception as e:
+        log.error(f"DMのメッセージ送信中にエラーが発生しました。\n{e}")
+
+@tasks.loop(seconds=15)
+async def subscribe_dm():
+    log = get_log("subscribe_dm")
+    try:
+        response = (
+            await supabase.table("dm")
+            .select("id, title, member, time")
+            .contains("member", [str(currentUser["id"])])
+            .order("time", desc=True)
+            .execute()
+        )
+        dmids = []
+        for i in response.data:
+            dmids.append(i["id"])
+        response2 = (
+            await supabase.table("dm")
+            .select("id, title, post, member, host_id")
+            .in_("id", dmids)
+            .execute()
+        )
+        response3 = await supabase.rpc("get_all_unread_dm_counts", {"p_user_id": currentUser["id"]}).execute()
+        #log.debug(response3)
+        if(response3.data):
+            unread_data = {}
+            for i in response3.data:
+                unread_data[i["dm_id"]] = i["unread_count"]
+            log.debug(unread_data)
+            for i in response2.data:
+                #log.debug(i)
+                if(i["id"] not in unread_data):
+                    continue
+                ranges = unread_data[i["id"]] * -1
+                for a in i["post"][ranges:]:
+                    log.debug(a)
+                    if("type" not in a):
+                        log.debug("suc")
+                        await handle_dm_message(a, i["id"])
+                await supabase.rpc("mark_all_dm_messages_as_read", {"p_dm_id": i["id"], "p_user_id": currentUser["id"]}).execute()
+    except Exception as e:
+        log.error(f"DMの処理中にエラーが発生しました。\n{e}")
+
 async def handle_notification_message(notification):
     log = get_log("handle_notification_message")
     try:
         if("あなたをDMに招待しました" in notification["message"]):
             dmid = notification["open"][4:]
-            message = {
-                "id": str(uuid.uuid4()),
-                "time": datetime.now(timezone.utc).isoformat(timespec="microseconds"),
-                "userid": currentUser["id"],
-                "content": dmInviteMessage,
-                "attachments": [],
-                "read": [currentUser["id"]]
-            }
-            response = (
-                await supabase.rpc("append_to_dm_post", {
-                    "dm_id_in": dmid,
-                    "new_message_in": message
-                })
-                .execute()
-            )
+            await send_dm_message(dmid, dmInviteMessage)
+        elif("あなたをメンションしました" in notification["message"]):
+            postid = notification["open"][6:]
+            if("@4332さん" in notification["message"]):
+            #     response = (
+            #         await supabase.rpc("handle_like", {
+            #             "p_post_id": postid
+            #         })
+            #         .execute()
+            #     )
+                response = (
+                    await supabase.table("post")
+                    .select("content, repost_to")
+                    .eq("id", postid)
+                    .execute()
+                )
+                if("/finish" in response.data[0]["content"]):
+                    await supabase.rpc("handle_like", {"p_post_id": postid}).execute()
+                    #console_task.cancel()
+                    #await bot.close()
     except Exception as e:
         log.error(f"通知のメッセージ処理中にエラーが発生しました\n{e}")
 
+async def handle_dm_message(msg,dmid):
+    match msg["content"]:
+        case "/hello":
+            await send_dm_message(dmid, "こんにちは!NyaXBotです!")
+        case "/help":
+            await send_dm_message(dmid, helpMessage)
+
+async def status_update(status):
+    log = get_log("status_update")
+    try:
+        message = f"""NyaXBot
+管理者:あおく
+https://github.com/aoku151/nyax-bot/
+
+ステータス:{status}"""
+        updatedData = {
+            "name": "非公式NyaXBot",
+            "me": message,
+            "settings": {
+                "show_like": False,
+                "show_follow": True,
+                "show_follower": True,
+                "show_star": False,
+                "show_scid": False,
+                "default_timeline_tab": "all",
+                "emoji": "emojione",
+                "theme": "light"
+            },
+            "icon_data": currentUser["icon_data"]
+        }
+        response = (
+            await supabase.table("user")
+            .update(updatedData)
+            .eq("id", currentUser["id"])
+            .execute()
+        )
+        return response
+    except Exception as e:
+        log.error(f"プロフィールの更新中にエラーが発生しました。\n{e}")
+
 async def main():
     log = main_log
-    global currentUser, supabase, session
+    global currentUser, supabase, session, console_task
     try:
         supabase, session = await sessions.get_supabase()
         # session = await supabase.auth.get_session()
@@ -76,16 +180,21 @@ async def main():
 
         @bot.event
         async def on_ready():
-            global log_channel
+            global log_channel, console_task
+            console_task = asyncio.create_task(console_input())
             log.info(f"Discord:{bot.user}としてログインしました^o^")
             try:
                 synced = await bot.tree.sync()
                 log.info(f"{len(synced)}個のコマンドを同期しました。")
                 log_channel = bot.get_guild(1440680053867286560).get_channel(1440680171890933863)
-                await handle_notification()
             except Exception as e:
                 log.error(f"コマンドの同期中にエラーが発生しました。\n{e}")
-            asyncio.create_task(console_input())
+            try:
+                await handle_notification()
+                subscribe_dm.start()
+                await status_update("起動中")
+            except Exception as e:
+                log.error(f"初期動作の実行中にエラーが発生しました。\n{e}")
             # await supabase.rpc("create_post", {"p_content": "NyaXBotが起動しました!", "p_reply_id": None, "p_repost_to": None, "p_attachments": None}).execute()
             await log_channel.send("NyaXBotが起動しました!")
 
