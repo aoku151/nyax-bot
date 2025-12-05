77dadf6bacdf12bd53f96fc201e44daff4edf3ab
あおく <guratan.151.123@gmail.com>
2025-12-05 11:05:14 +0900

    コメント

--- func/data.py
+++ func/data.py
@@ -1,15 +1,19 @@
+# まとめる必要がないヘッダー
 header = {"Content-Type": "application/json"}
 
+# DMで使えるコマンドのリスト
 dmCommandList = [
     "/hello",
     "/help"
 ]
 
+# 処理(変更禁止)
 dmCommands = ""
 if len(dmCommandList) != 0:
     for i in dmCommandList:
         dmCommands += f"{i}\n"
 
+# DMに招待された時のメッセージ
 dmInviteMessage = f"""\
 NyaXBotへのDMが開通しました! _veryx2-long-cat_
 開発者: @4332
@@ -18,6 +22,7 @@ Githubリポジトリ: https://github.com/aoku151/nyax-bot/
 使用出来るコマンド
 {dmCommands}"""
 
+# Helpコマンドの応答
 helpMessage = f"""\
 NyaXBot @1340
 開発者: @4332
