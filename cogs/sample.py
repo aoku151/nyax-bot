5b9c77c94e611155cb53f811b5d0988ffebe4de3
aoku_151 <guratan.151.123@gmail.com>
2025-12-11 17:42:11 +0900

    Create sample.py

---
+++ cogs/sample.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+from func.log import get_log
+
+class NameCog(commands.Cog):
+    def __init__(self, bot:commands.Bot):
+        self.bot = bot
+        self.log = get_log(self.__class__.__name__)
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.log.info(f"{self.__class__.__name__}が読み込まれました！")
+	
+async def setup(bot):
+    await bot.add_cog(NameCog(bot))
