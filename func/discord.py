7d73029ec1b04823f4ea77dd0ef44c14da929c10
aoku_151 <guratan.151.123@gmail.com>
2025-12-11 21:57:57 +0900

    Update discord.py

--- func/discord.py
+++ func/discord.py
@@ -3,8 +3,7 @@ from discord.ext import commands
 from discord import app_commands
 
 class MyBot(commands.Bot):
-    def __init__(self, supabase, supabase_session, **options):
+    def __init__(self, **options):
         super().__init__(**options)
-        self.supabase = supabase
-        self.supabase_session = supabase_session
-        
+        self.supabase = None
+        self.supabase_session = None
