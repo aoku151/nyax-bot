82892687cc5feda23f7552138315ac08c8b43f22
aoku151 <guratan.151.123@gmail.com>
2025-12-14 11:45:41 +0900

    Cog

--- cogs/sample.py
+++ cogs/sample.py
@@ -7,12 +7,11 @@ class NameCog(commands.Cog):
     def __init__(self, bot:commands.Bot):
         self.bot = bot
         self.log = get_log(self.__class__.__name__)
-		self.supabase = bot.supabase
-		self.session = bot.supabase_session
-    
+        self.supabase = bot.supabase
+        self.session = bot.supabase_session
     @commands.Cog.listener()
     async def on_ready(self):
         self.log.info(f"{self.__class__.__name__}が読み込まれました！")
-	
+
 async def setup(bot):
     await bot.add_cog(NameCog(bot))
