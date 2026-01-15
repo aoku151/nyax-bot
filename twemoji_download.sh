6ac8bf8ef0adae42e84979fa3b7213134267aeba
aoku_151 <guratan.151.123@gmail.com>
2026-01-15 20:03:22 +0900

    Update twemoji_download.sh

--- twemoji_download.sh
+++ twemoji_download.sh
@@ -2,14 +2,25 @@
 set -e
 
 DEST_DIR="twemoji/72x72"
+TMP_DIR="twemoji_tmp"
+
+echo "Downloading jdecked/twemoji ZIP..."
+
+# ZIP をダウンロード
+wget -O twemoji.zip \
+  https://github.com/jdecked/twemoji/archive/refs/heads/main.zip
+
+echo "Unzipping..."
+unzip -q twemoji.zip -d "$TMP_DIR"
+
+echo "Extracting PNGs..."
 mkdir -p "$DEST_DIR"
 
-echo "Downloading Twemoji PNGs (jdecked/twemoji)..."
+# PNG をコピー
+cp "$TMP_DIR"/twemoji-main/assets/72x72/*.png "$DEST_DIR"/
 
-wget -r -np -nH --cut-dirs=3 \
-  -A png \
-  https://github.com/jdecked/twemoji/raw/main/assets/72x72/ \
-  -P "$DEST_DIR"
+echo "Cleaning up..."
+rm -rf "$TMP_DIR" twemoji.zip
 
 echo "Done!"
 echo "Saved to: $DEST_DIR"
