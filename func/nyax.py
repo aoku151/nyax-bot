c6b51929420a6ef3318d72625ae868e301281ac7
aoku_151 <guratan.151.123@gmail.com>
2026-02-10 14:25:01 +0900

    Update nyax.py

--- func/nyax.py
+++ func/nyax.py
@@ -8,6 +8,7 @@ from pydantic import BaseModel
 load_dotenv()
 
 class Setting_Data(BaseModel):
+    
     show_like:bool
     show_follow:bool
     show_follower:bool
@@ -17,6 +18,23 @@ class Setting_Data(BaseModel):
     default_timeline_tab:Literal["all", "foryou", "following"]
     emoji:Literal["twemoji", "emojione", "default"]
     theme:literal["auto", "light", "dark"]
+
+class CurrentUser:
+    def __init__(self):
+        self.id:int = None
+        self.uuid:str = None
+        self.scid:str = None
+        self.name:str = None
+        self.me:str = None
+        self.icon_data:str = None
+        self.settings
+        self.pin:str = None
+        self.like:list[str] = None
+        self.star:list[str] = None
+        self.follow:list[int] = None
+        self.block:list[int] = None
+        self.admin:bool = None
+        self.notice
     
 
 class NyaXClient:
